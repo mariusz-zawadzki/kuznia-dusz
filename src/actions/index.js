e46eca97a1e747031444b40ee7f16d3d61803ee5
monku <mariusz.r.zawadzki@gmail.com>
2017-10-25 00:28:08 +0200

    add image upload and preview for items

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,20 +7,20 @@ import * as types from './types'
 export function signOutUser() {
     return (dispatch, getState) => {
         firebase.auth().signOut()
-        .then(() => {
-            let listeners = getState().listeners || [];
-            listeners.forEach((e)=>e());
-            dispatch(
-                {
-                    type: types.REMOVE_LISTENERS
-                }
-            );
-            dispatch(
-                {
-                    type: types.SIGN_OUT
-                }
-            );
-        })
+            .then(() => {
+                let listeners = getState().listeners || [];
+                listeners.forEach((e) => e());
+                dispatch(
+                    {
+                        type: types.REMOVE_LISTENERS
+                    }
+                );
+                dispatch(
+                    {
+                        type: types.SIGN_OUT
+                    }
+                );
+            })
     }
 }
 
@@ -45,23 +45,137 @@ export function saveItem(itemData) {
 
     return (dispatch, getState) => {
         const item = { ...itemData };
-
-
-        const newItem = firebase
-            .firestore()
-            .collection(`games/${item.gameId}/items`)
-            .doc(item.id);
-        item.id = newItem.id;
-        newItem
-            .set(item)
-            .then(() => {
-                dispatch(
-                    {
-                        type: types.SAVE_ITEM,
-                        payload: item
-                    }
-                );
+        console.log(typeof item.image)
+        console.log(item);
+        let storageRef = firebase.storage().ref();
+        // let mountainsRef = 
+        storageRef.child('/games/' + item.gameId + '/image.jpg')
+            .put(item.image[0])
+            .then((snapshot) => {
+                console.log(snapshot);
+                const newItem = firebase
+                    .firestore()
+                    .collection(`games/${item.gameId}/items`)
+                    .doc(item.id);
+                item.id = newItem.id;
+                item.image = {
+                    id:snapshot.metadata.fullPath,
+                    link: snapshot.downloadURL
+                }
+                newItem
+                    .set(item)
+                    .then(() => {
+                        dispatch(
+                            {
+                                type: types.SAVE_ITEM,
+                                payload: item
+                            }
+                        );
+                    })
+                    .catch((e) => {
+                        console.log("ERROR saving item", e)
+                    })
+                debugger;
+                // snapshot: UploadTaskSnapshot
+                // bytesTransferred
+                // :
+                // 238032
+                // downloadURL
+                // :
+                // "https://firebasestorage.googleapis.com/v0/b/kuznia-dusz.appspot.com/o/games%2F0EusMqJ3veoyImGhrET3%2Fimage.jpg?alt=media&token=91bcf3d3-78b4-458a-aa91-893345fc680d"
+                // metadata
+                // :
+                // bucket
+                // :
+                // "kuznia-dusz.appspot.com"
+                // cacheControl
+                // :
+                // undefined
+                // contentDisposition
+                // :
+                // "inline; filename*=utf-8''image.jpg"
+                // contentEncoding
+                // :
+                // "identity"
+                // contentLanguage
+                // :
+                // undefined
+                // contentType
+                // :
+                // "image/jpeg"
+                // customMetadata
+                // :
+                // undefined
+                // downloadURLs
+                // :
+                // Array(1)
+                // 0
+                // :
+                // "https://firebasestorage.googleapis.com/v0/b/kuznia-dusz.appspot.com/o/games%2F0EusMqJ3veoyImGhrET3%2Fimage.jpg?alt=media&token=91bcf3d3-78b4-458a-aa91-893345fc680d"
+                // length
+                // :
+                // 1
+                // __proto__
+                // :
+                // Array(0)
+                // fullPath
+                // :
+                // "games/0EusMqJ3veoyImGhrET3/image.jpg"
+                // generation
+                // :
+                // "1508448592962091"
+                // md5Hash
+                // :
+                // "8CrDQQxximHs/HvqxhI38w=="
+                // metageneration
+                // :
+                // "1"
+                // name
+                // :
+                // "image.jpg"
+                // size
+                // :
+                // 238032
+                // timeCreated
+                // :
+                // "2017-10-19T21:29:52.663Z"
+                // type
+                // :
+                // "file"
+                // updated
+                // :
+                // "2017-10-19T21:29:52.663Z"
+                // ref
+                // :
+                // (...)
+                // get ref
+                // :
+                // ƒ generateRef()
+                // __proto__
+                // :
+                // Object
+                // const newItem = firebase
+                //     .firestore()
+                //     .collection(`games/${item.gameId}/items`)
+                //     .doc(item.id);
+                // item.id = newItem.id;
+                // newItem
+                //     .set(item)
+                //     .then(() => {
+                //         dispatch(
+                //             {
+                //                 type: types.SAVE_ITEM,
+                //                 payload: item
+                //             }
+                //         );
+                //     })
+                //     .catch((e) => {
+                //         console.log("ERROR saving item", e)
+                //     })
             })
+            .catch((e) => {
+                console.log("ERROR saving item file", e)
+            });
     };
 }
 export function saveCharacter(characterData) {
@@ -82,6 +196,9 @@ export function saveCharacter(characterData) {
                         payload: character
                     }
                 );
+            })
+            .catch((e) => {
+                console.log("ERROR saving character", e)
             })
     };
 }
--- src/components/items/ItemNew.js
+++ src/components/items/ItemNew.js
@@ -5,24 +5,61 @@ import * as actions from '../../actions'
 import MyCKEditor from '../editor/ckeditor'
 
 
-const FileUpload = ({ input, type }) =>{
+const FileUpload = (props) =>{
+    console.log(props)
+    const { input, type } = props;
     input.value = undefined;
-  return <input type={type} {...input} />
+  return <input type={type} {...input} onChange={event => {
+    input.onChange(event)
+    getBase64(event.target.files[0], (data)=>props.updatePreview(data));
+  }}/>
 }
-
+function getBase64(file, onload) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = onload;
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+ }
 
 class ItemNew extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.updateImage.bind(this);
+        // this.state={
+            // image
+        // }
+    }
+
+    updateImage(data){
+        debugger;
+        console.log("Update image this",this,data);
+        this.imageInput.src = data.target.result;
+    }
+
 
     submit(values) {
-        console.log(values);
-        // const gameId = this.props.match.params.gameId;
         this.props.saveItem(values);
         this.props.history.push(`/games/${values.gameId}/items`)
     }
 
     render() {
-        const { history } = this.props;
+        const { history,item } = this.props;
+        let preview = "";
+        // if(this.props.image){
+            console.log(this.props)
+        // }
+        if(item && item.image && item.image.link)
+        {
+            console.log(item.image)
+            preview = <img style={{width:"100%"}} alt={item.name} src={item.image.link} ref={(input) => { 
+                this.imageInput = input; 
+                console.log("this",this)
+                }
+                } />
+        }
         return (
             <div className="component-character-new center">
                 <form onSubmit={this.props.handleSubmit(this.submit.bind(this))}>
@@ -48,14 +85,16 @@ class ItemNew extends React.Component {
                             placeholder="Opis" />
                     </div>
 
-                    {/* <div className="form-group">
+                    <div className="form-group">
                         <label>Zdjęcie: </label>
                         <Field 
                             className="form-control"
                             name="image" 
-                            component={FileUpload} type="file" />
+                            component={FileUpload}
+                             type="file"
+                             updatePreview={(d)=>this.updateImage(d)} />
+                    </div>
 
-                    </div> */}
                     <div className="buttons">
                         <button className="btn btn-primary" type="submit">Zapisz</button>
                         <button className="btn btn-danger" type="button" onClick={(e) => {
@@ -63,6 +102,7 @@ class ItemNew extends React.Component {
                         }}>Wróć</button>
                     </div>
                 </form>
+                        {preview}
             </div>
         );
     }
@@ -73,12 +113,14 @@ const mapStateToProps = (state, ownProps) => {
         const gameId = ownProps.match.params.gameId;
         const itemId =  ownProps.match.params.id;
         const gameItems = state.items[gameId] || {ids:[],map:{}};
+        const item = gameItems.map[itemId];
         let newprops = {
             // properly match initial values8
             initialValues: {
-                ...gameItems.map[itemId],
+                ...item,
                 gameId
-            }
+            },
+            item
         }
         return newprops;
     }
